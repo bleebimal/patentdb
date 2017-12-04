a484d4bdc5bd84554d10b5d4022bdad877dd6251
sameer <sameer.koirala@deerwalkcompware.com>
2017-12-04 18:13:08 +0545

    Added predefined query with which generated where conditions are attached

--- grails-app/controllers/com/pathunt/HomeController.groovy
+++ grails-app/controllers/com/pathunt/HomeController.groovy
@@ -8,24 +8,12 @@ class HomeController {
     def springSecurityService
     def homeService
     def queryGeneratorService
-    def sqlQuery = "SELECT concat(p.country,p.number) as 'Publication Number', " +
-                   "p.title, p.abstract, p.first_claim, year(p.date), p.date, " +
-                   "concat(ir.name_first, ' ', ir.name_last) as 'Inventor(s), " +
-                   "concat(a.organization,a.name_first, ' ', a.name_last) as 'Assignee(s)," +
-                   "u.subclass_id as 'All_UPC'" + "concat(i.section, i.ipc_class, " +
-                   "i.subclass, i.main_group, '/', i.subgroup as 'All_IPC'" +
-                   "p.citation3 as 'cited_by_within_3_years' FROM patent p " +
-                   "INNER JOIN inventor ir ON p.country = ir.country AND p.id = ir.patent_id " +
-                   "INNER JOIN assignee a ON p.country = a.country AND p.id = a.patent_id " +
-                   "INNER JOIN uspc u ON p.country = u.country AND p.id = u.patent_id " +
-                   "INNER JOIN ipcr i ON p.country = i.country AND p.id = i.patent_id " +
-                   "WHERE "
+    def query = ""
     def index() {
         if(springSecurityService.isLoggedIn()) {
             if(SpringSecurityUtils.ifAllGranted('ROLE_ADMIN')) {
                 def currentUser = springSecurityService.getCurrentUser()
-                render view: 'index', model: [currentUser:currentUser, sqlQuery:sqlQuery]
-                sqlQuery = ""
+                render view: 'index', model: [currentUser:currentUser, sqlQuery:query]
             }
         }
         else {
@@ -38,11 +26,23 @@ class HomeController {
 //        println "parser"
         String preQuery = "[" + homeService.translate(params.query) + "]"
         println "preQuery = $preQuery"
-        println "gfdhgfjhghfgdgflkygyktf"
         String prefix = homeService.prefixConverter(preQuery)
         queryGeneratorService.setPrefixQuery(prefix)
-        sqlQuery += queryGeneratorService.parseQuery()
-        println sqlQuery
+        query = ""
+        query += "SELECT concat(p.country,p.number) as 'Publication Number', " +
+                "p.title, p.abstract, p.first_claim, year(p.date), p.date, " +
+                "concat(ir.name_first, ' ', ir.name_last) as 'Inventor(s)', " +
+                "concat(a.organization,a.name_first, ' ', a.name_last) as 'Assignee(s)'," +
+                "u.subclass_id as 'All_UPC', " + "concat(i.section, i.ipc_class, " +
+                "i.subclass, i.main_group, '/', i.subgroup) as 'All_IPC', " +
+                "p.citation3 as 'cited_by_within_3_years' FROM patent p " +
+                "INNER JOIN inventor ir ON p.country = ir.patent_country AND p.id = ir.patent_id " +
+                "INNER JOIN assignee a ON p.country = a.patent_country AND p.id = a.patent_id " +
+                "INNER JOIN uspc u ON p.country = u.patent_country AND p.id = u.patent_id " +
+                "INNER JOIN ipcr i ON p.country = i.patent_country AND p.id = i.patent_id " +
+                "WHERE "
+        query += queryGeneratorService.parseQuery()
+        println query
         redirect(action: 'index')
     }
 }
