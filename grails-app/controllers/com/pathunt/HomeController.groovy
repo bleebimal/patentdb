58a682716e400e4110fbfb32a7d10d1abbfc1268
sameer <sameer.koirala@deerwalkcompware.com>
2018-02-22 13:00:47 +0545

    Added function to store result unless logged out or cleared.

--- grails-app/controllers/com/pathunt/HomeController.groovy
+++ grails-app/controllers/com/pathunt/HomeController.groovy
@@ -20,9 +20,9 @@ class HomeController {
     def joinCPC = "INNER JOIN cpc c ON p.country = c.patent_country AND p.id = c.patent_id "
     def joinApplication = "INNER JOIN application ap ON p.country = ap.patent_country AND p.id = ap.patent_id "
     def joinCitation = "INNER JOIN uspatentcitation ct ON p.country = ct.patent_country AND p.id = ct.patent_id "
-    def joinLocation = "INNER JOIN location l ON p.country = l.patent_country AND p.id = l.patent_id "
+//    def joinLocation = "INNER JOIN location l ON p.country = l.patent_country AND p.id = l.patent_id "
 
-    def userInput = new HashMap<String,Query>()
+    def static userInput = new HashMap<String,Query>()
 
     def index() {
         if(springSecurityService.isLoggedIn()) {
@@ -39,11 +39,6 @@ class HomeController {
                     Map labels = ["patent_number": "Publication Number", "title": "Title",
                                   "abs":"Abstract", "year":"Publication Year", "date":"Publication Date", "first_claim":"First Claim", "inventor":"Inventor(s)", "assignee":"Assignee(s)",
                                   "ipc":"IPC(s)", "upc":"UPC(s)", "cpc":"CPC(s)", "citedby3":"Cited By (3 Years)", "cites":"Cites"]
-//                    def upperCase = { value ->
-//                        return value.toUpperCase()
-//                    }
-//                    Map formatters = [patent_number: upperCase]
-//                    Map parameters = [title: "Cool books"]
 
                     def lists = inputQuery.result
                     exportService.export(params?.extension, response.outputStream,lists, fields, labels, [:],[:])
@@ -71,16 +66,6 @@ class HomeController {
         }
     }
 
-//    def download() {
-//        if(springSecurityService.isLoggedIn()) {
-//            if(SpringSecurityUtils.ifAllGranted('ROLE_ADMIN')) {
-//
-//                }
-//        }
-//        else {
-//            redirect(controller: 'login', action: 'auth')
-//        }
-//    }
     def clear(){
         def currentUser = springSecurityService.getCurrentUser()
         userInput.remove(currentUser)
@@ -89,119 +74,90 @@ class HomeController {
     }
 
     def parser(){
-            //        homeService.parseQuery(params.query)
-//        println "parser"
         def lists = new ArrayList<Patent>()
 
         def user = springSecurityService.getCurrentUser()
         def userQuery = new Query()
         userQuery.query = params.query
         userQuery.isActive = true
-//        println input
-            String prefix = homeService.translate(userQuery.query)
+        userInput.put(user,userQuery)
+        String prefix = homeService.translate(userQuery.query)
 //        println "preQuery = $prefix"
-            queryGeneratorService.setPrefixQuery(prefix)
+        queryGeneratorService.setPrefixQuery(prefix)
 
-            /*query += "SELECT concat(p.country,p.number) as 'Publication Number', " +
-                    "p.title, p.abstract, p.first_claim, year(p.date), p.date, " +
-                    "concat(ir.name_first, ' ', ir.name_last) as 'Inventor(s)', " +
-                    "concat(a.organization,a.name_first, ' ', a.name_last) as 'Assignee(s)'," +
-                    "u.subclass_id as 'All_UPC', " + "concat(i.section, i.ipc_class, " +
-                    "i.subclass, i.main_group, '/', i.subgroup) as 'All_IPC', " +
-                    "p.citation3 as 'cited_by_within_3_years' FROM patent p " +
-                    "INNER JOIN inventor ir ON p.country = ir.patent_country AND p.id = ir.patent_id " +
-                    "INNER JOIN assignee a ON p.country = a.patent_country AND p.id = a.patent_id " +
-                    "INNER JOIN uspc u ON p.country = u.patent_country AND p.id = u.patent_id " +
-                    "INNER JOIN ipcr i ON p.country = i.patent_country AND p.id = i.patent_id " +
-                    "INNER JOIN cpc c ON p.country = c.patent_country AND p.id = c.patent_id " +
-                    "INNER JOIN application ap ON p.country = ap.patent_country AND p.id = ap.patent_id " +
-                    "INNER JOIN citation ct ON p.country = ct.patent_country AND p.id = ct.patent_id " +
-                    "WHERE "*/
-            def query = "SELECT concat(p.country,p.id) as 'publication_number', " +
-                    "p.title, p.abstract, year(p.date) as year, p.date, " +
-                    "p.first_claim, "+
-                    "p.inventors, " +
-                    "p.assignees, " +
-                    "p.upc, " + "p.ipc, " + "p.cpc, " +
-                    "p.citedby3, " + "p.cites " +
-                    "FROM patentfinal p "
-        println "query = $query"
-            def whereClause = queryGeneratorService.parseQuery()
+        def query = "SELECT concat(p.country,p.id) as 'publication_number', " +
+                "p.title, p.abstract, year(p.date) as year, p.date, " +
+                "p.first_claim, "+
+                "p.inventors, " +
+                "p.assignees, " +
+                "p.upc, " + "p.ipc, " + "p.cpc, " +
+                "p.citedby3, " + "p.cites " +
+                "FROM patentfinal p "
+        def whereClause = queryGeneratorService.parseQuery()
 //        println "whereClause = $whereClause"
-            if (whereClause.contains("ERROR")){
-                def message = whereClause.split(":")
-                println "Error message = " + message[1]
-                flash.message = "query.invalid.message"
-                flash.args = [message[1]]
-                flash.default = "invalid query"
-                queryGeneratorService.reset()
-            }
-            else {
-                def whereQuery = "WHERE " + whereClause
-                def tableList = queryGeneratorService.tables
+        if (whereClause.contains("ERROR")){
+            def message = whereClause.split(":")
+            println "Error message = " + message[1]
+            flash.message = "query.invalid.message"
+            flash.args = [message[1]]
+            flash.default = "invalid query"
+            queryGeneratorService.reset()
+        }
+        else {
+            def whereQuery = "WHERE " + whereClause
+            def tableList = queryGeneratorService.tables
 
-                for (String table in tableList) {
-                    if (table == "inventor") {
-                        query += joinInventor
-                        //                println query
-                    } else if (table == "assignee") {
-                        query += joinAssignee
-                        //                println query
-                    } else if (table == "upc") {
-                        query += joinUPC
-                        //                println query
-                    } else if (table == "ipc") {
-                        query += joinIPC
-                        //                println query
-                    } else if (table == "cpc") {
-                        query += joinCPC
-                        //                println query
-                    } else if (table == "application") {
-                        query += joinApplication
-                        //                println query
-                    } else if (table == "citation") {
-                        query += joinCitation
-                        //                println query
-                    }
-                }
-                queryGeneratorService.reset()
-                query += whereQuery
-                println "query " + query
-                def sql = new Sql(dataSource)
-                def data = sql.rows(query)
-                sql.close()
-                println "data size " + data.size()
-                if (data.size() == 0){
-                    flash.message = "result.empty.message"
-                    flash.default = "empty result"
-                }
-                data.each {
-                    Patent patentdemo = new Patent()
-                    patentdemo.patent_number = it.publication_number
-                    patentdemo.title = it.title
-                    patentdemo.abs = it.abstract
-                    patentdemo.year = it.year
-                    patentdemo.date = it.date
-                    patentdemo.first_claim = it.first_claim
-                    patentdemo.assignee = it.assignees
-                    patentdemo.inventor = it.inventors
-                    patentdemo.ipc = it.ipc
-                    patentdemo.upc = it.upc
-                    patentdemo.cpc = it.cpc
-                    patentdemo.citedby3 = it.citedby3
-                    patentdemo.cites = it.cites
-
-                    lists.add(patentdemo)
+            for (String table in tableList) {
+                if (table == "inventor") {
+                    query += joinInventor
+                } else if (table == "assignee") {
+                    query += joinAssignee
+                } else if (table == "upc") {
+                    query += joinUPC
+                } else if (table == "ipc") {
+                    query += joinIPC
+                } else if (table == "cpc") {
+                    query += joinCPC
+                } else if (table == "application") {
+                    query += joinApplication
+                } else if (table == "citation") {
+                    query += joinCitation
                 }
             }
+            queryGeneratorService.reset()
+            query += whereQuery
+            println "query " + query
+            def sql = new Sql(dataSource)
+            def data = sql.rows(query)
+            sql.close()
+            println "data size " + data.size()
+            if (data.size() == 0){
+                flash.message = "result.empty.message"
+                flash.default = "empty result"
+            }
+            data.each {
+                Patent patentdemo = new Patent()
+                patentdemo.patent_number = it.publication_number
+                patentdemo.title = it.title
+                patentdemo.abs = it.abstract
+                patentdemo.year = it.year
+                patentdemo.date = it.date
+                patentdemo.first_claim = it.first_claim
+                patentdemo.assignee = it.assignees
+                patentdemo.inventor = it.inventors
+                patentdemo.ipc = it.ipc
+                patentdemo.upc = it.upc
+                patentdemo.cpc = it.cpc
+                patentdemo.citedby3 = it.citedby3
+                patentdemo.cites = it.cites
+
+                lists.add(patentdemo)
+            }
+        }
         userQuery.result = lists
         userQuery.isActive = false
         userInput.put(user,userQuery)
-/*//        println data
-//        lists.clear()
-
-        println "lists.size() = $lists.size"*/
-            redirect( action: 'index')
+        redirect( action: 'index')
     }
 
     def changePassword(){
---
+++ grails-app/domain/com/pathunt/Query.groovy
@@ -0,0 +1,8 @@
+package com.pathunt
+
+class Query {
+
+    String query
+    ArrayList<Patent> result
+    boolean isActive = false
+}
