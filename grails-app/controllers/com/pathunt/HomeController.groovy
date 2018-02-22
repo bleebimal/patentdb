f5ebe1712d7e2bed6787013a77ffbca6bc827a65
Bimal <bimal.blee@gmail.com>
2018-02-22 11:25:32 +0545

    Added citedby3 and cites in export.

--- grails-app/controllers/com/pathunt/HomeController.groovy
+++ grails-app/controllers/com/pathunt/HomeController.groovy
@@ -35,10 +35,10 @@ class HomeController {
                     response.contentType = grailsApplication.config.grails.mime.types[params.extension]
                     response.setHeader("Content-disposition", "attachment; filename=output.${params.extension}")
 
-                    List fields = ["patent_number", "title", "abs", "year", "date", "first_claim", "inventor", "assignee", "ipc", "upc", "cpc", "citation3"]
+                    List fields = ["patent_number", "title", "abs", "year", "date", "first_claim", "inventor", "assignee", "ipc", "upc", "cpc", "citedby3", "cites"]
                     Map labels = ["patent_number": "Publication Number", "title": "Title",
                                   "abs":"Abstract", "year":"Publication Year", "date":"Publication Date", "first_claim":"First Claim", "inventor":"Inventor(s)", "assignee":"Assignee(s)",
-                                  "ipc":"IPC(s)", "upc":"UPC(s)", "cpc":"CPC(s)", "citaion3":"Cited By (3 Years)"]
+                                  "ipc":"IPC(s)", "upc":"UPC(s)", "cpc":"CPC(s)", "citedby3":"Cited By (3 Years)", "cites":"Cites"]
 //                    def upperCase = { value ->
 //                        return value.toUpperCase()
 //                    }
@@ -112,12 +112,13 @@ class HomeController {
                     "WHERE "*/
             def query = "SELECT concat(p.country,p.id) as 'publication_number', " +
                     "p.title, p.abstract, year(p.date) as year, p.date, " +
-                    "p.first_claim,"+
-                    "p.inventors," +
-                    "p.assignees," +
+                    "p.first_claim, "+
+                    "p.inventors, " +
+                    "p.assignees, " +
                     "p.upc, " + "p.ipc, " + "p.cpc, " +
-                    "p.citedby3 " +
+                    "p.citedby3, " + "p.cites " +
                     "FROM patentfinal p "
+        println "query = $query"
             def whereClause = queryGeneratorService.parseQuery()
 //        println "whereClause = $whereClause"
             if (whereClause.contains("ERROR")){
@@ -180,7 +181,8 @@ class HomeController {
                     patentdemo.ipc = it.ipc
                     patentdemo.upc = it.upc
                     patentdemo.cpc = it.cpc
-                    patentdemo.citation3 = it.citedby3
+                    patentdemo.citedby3 = it.citedby3
+                    patentdemo.cites = it.cites
 
                     lists.add(patentdemo)
                 }
@@ -208,7 +210,7 @@ class HomeController {
             flash.args = [status]
             flash.default = "Unable to change password."
         }
-        redirect( action: "index", model: [currentUser:user])
+        redirect( controller:"home", action: "index", model: [currentUser:user])
     }
 
     /*This method checks whether the new password that the user wants to change to is valid*/
